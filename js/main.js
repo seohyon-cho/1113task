762bdd139ab93e647e8f3488a90cbd5c16ae5211
seohyon-cho <flanique@naver.com>
2023-11-13 20:20:22 +0900

    미션 (3) - 이벤트문, 활성화함수, 박스색상 변경함수로 분리해서 구현

--- js/main.js
+++ js/main.js
@@ -1,12 +1,21 @@
 const btns = document.querySelectorAll('li');
 const box = document.querySelector('.box');
 
+// 이벤트문 + 활성화함수 + 박스색상 변경함수로 분리해서 구현
+
+// 활성화 함수
+function activation(btns, idx) {
+	btns.forEach((btn) => btn.classList.remove('on'));
+	btns[idx].classList.add('on');
+}
+// 박스색상 변경함수
+function boxColor(btns, idx) {
+	box.style.backgroundColor = btns[idx].innerText;
+}
+// 이벤트문
 btns.forEach((btn, idx) => {
-	btn.addEventListener('click', (e) => {
-		// 버튼 활성화 처리
-		btns.forEach((btn) => btn.classList.remove('on'));
-		btns[idx].classList.add('on');
-		// 박스 색상
-		box.style.backgroundColor = e.currentTarget.innerText;
+	btn.addEventListener('click', () => {
+		activation(btns, idx);
+		boxColor(btns, idx);
 	});
 });
