2c0aff101c8a500b6144cf0d25a8eb8833097fe1
seohyon-cho <flanique@naver.com>
2023-11-13 19:30:09 +0900

    미션 (2) - 각 버튼 클릭 시 버튼 안쪽의 텍스트 내용으로 박스 배경색 변경

--- js/main.js
+++ js/main.js
@@ -2,8 +2,11 @@ const btns = document.querySelectorAll('li');
 const box = document.querySelector('.box');
 
 btns.forEach((btn, idx) => {
-	btn.addEventListener('click', () => {
+	btn.addEventListener('click', (e) => {
+		// 버튼 활성화 처리
 		btns.forEach((btn) => btn.classList.remove('on'));
 		btns[idx].classList.add('on');
+		// 박스 색상
+		box.style.backgroundColor = e.currentTarget.innerText;
 	});
 });
